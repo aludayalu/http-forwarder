5be81dbd190c8ffe2d51e0fe21e76dd1de7aec3c
aludayalu <pl46u3.ducklinz@gmail.com>
2024-04-27 12:12:00 +0530

    added proxy

---
+++ main.py
@@ -0,0 +1,11 @@
+import flask, requests
+from flask import Flask, request
+
+app=Flask(__name__)
+
+url="http://0.tcp.in.ngrok.io:17861"
+
+@app.get("/")
+def home():
+    res=requests.post(url, files=dict(request.files), headers=dict(request.headers))
+    return res.content
